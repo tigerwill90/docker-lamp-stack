7041c2d9527af0f47b7127971e1fc1ea2f9ae4eb
Sylvain <sylvain.muller90@gmail.com>
2018-06-28 22:48:48 +0200

    memcached test

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,11 @@
 <?php
-  echo apache_get_version();
-  phpinfo();
+  $m = new Memcached();
+  $m->addServer('localhost', 11211);
+
+  $reponse = $m->get('msg');
+  if (isset($response)) {
+    echo 'Cached msg : ' . $response;
+  } else {
+    $m->set('msg', 'memcached is working');
+    echo 'Msg setted';
+  }
